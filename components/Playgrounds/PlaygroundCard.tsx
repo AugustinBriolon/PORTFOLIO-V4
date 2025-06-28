b65ec0a675493b6a054bf4cb11c0dfac23664705
Augustin Briolon <augustin.briolon@gmail.com>
2025-06-28 23:23:37 +0200

    :art: amélioration du style des composants PlaygroundCard avec ajout de classes pour le mode sombre et ajustement des couleurs de texte

--- components/Playgrounds/PlaygroundCard.tsx
+++ components/Playgrounds/PlaygroundCard.tsx
@@ -13,16 +13,16 @@ export default function PlaygroundCard({
   return (
     <Link
       key={index}
-      className="group flex w-full cursor-pointer gap-4 border-t border-black/30 px-6 py-5 transition-colors hover:bg-black/5"
+      className="group flex w-full cursor-pointer gap-4 border-t border-black/30 px-6 py-5 transition-colors hover:bg-black/5 dark:border-white/30 dark:hover:bg-white/5"
       href={playground.websiteUrl}
       target="_blank"
       onMouseEnter={onMouseEnter}
     >
       <h2 className="w-full text-xl font-bold md:w-1/4 md:text-2xl">{playground.title}</h2>
-      <span className="hidden flex-1 truncate text-black/70 md:block">
+      <span className="hidden flex-1 truncate text-black/70 md:block dark:text-white/70">
         {playground.description}
       </span>
-      <span className="flex-1 text-right whitespace-nowrap text-black/90 transition-all duration-300 group-hover:visible md:invisible">
+      <span className="flex-1 text-right whitespace-nowrap text-black/90 transition-all duration-300 group-hover:visible md:invisible dark:text-white/90">
         See project
       </span>
     </Link>
