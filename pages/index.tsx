9478fc9eca8414b73ac49380eebef958b79c4c9c
Augustin Briolon <augustin.briolon@gmail.com>
2025-06-28 23:29:28 +0200

    :art: suppression du texte de présentation et réorganisation des sections "Statut" et "Contact" pour une meilleure lisibilité et accessibilité sur la page d'accueil.

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,6 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 export default function Home({ projects }: { projects: TypeProject[] }) {
-  const aboutText =
-    "Portfolio d'Augustin Briolon. Développeur web spécialisé en front-end. Je transforme vos projets en sites créatifs et performants.".split(
-      " ",
-    );
-
   const imagesArray = projects.map((project) => project.mainImage);
 
   const timelineProjectAnim = () => {
@@ -22,16 +17,6 @@ export default function Home({ projects }: { projects: TypeProject[] }) {
       duration: 1,
       ease: "power2.out",
     });
-    tl.from(
-      ".anim-text",
-      {
-        y: 100,
-        stagger: 0.01,
-        duration: 0.8,
-        ease: "power2.out",
-      },
-      "-=1",
-    );
     tl.from(
       ".bottom-anim",
       {
@@ -84,52 +69,40 @@ export default function Home({ projects }: { projects: TypeProject[] }) {
           </div>
         </div>
 
-        <div className="z-20 flex w-full flex-col justify-between gap-8">
-          <p className="about-text overflow-hidden">
-            {aboutText.map((word, index) => (
-              <span key={index} className="inline-block overflow-hidden">
-                <span className="anim-text inline-block">
-                  {word}
-                  {index !== aboutText.length - 1 && "\u00A0"}
-                </span>
+        <div className="z-20 flex w-full items-start justify-between gap-4">
+          <div>
+            <div className="flex items-center gap-2 overflow-hidden">
+              <p className="bottom-anim text-sm">Status</p>
+              <span className="ping-point relative flex h-2 w-2">
+                <span className="bg-green-pulse absolute inline-flex h-full w-full animate-ping rounded-full opacity-75"></span>
+                <span className="bg-green-pulse relative inline-flex h-2 w-2 rounded-full"></span>
               </span>
-            ))}
-          </p>
-          <div className="flex w-full items-start justify-between gap-4">
-            <div>
-              <div className="flex items-center gap-2 overflow-hidden">
-                <p className="bottom-anim text-sm">Status</p>
-                <span className="ping-point relative flex h-2 w-2">
-                  <span className="bg-green-pulse absolute inline-flex h-full w-full animate-ping rounded-full opacity-75"></span>
-                  <span className="bg-green-pulse relative inline-flex h-2 w-2 rounded-full"></span>
-                </span>
-              </div>
-              <div className="overflow-hidden">
-                <p className="bottom-anim flex items-center gap-2 font-medium uppercase">
-                  Disponible
-                </p>
-              </div>
             </div>
-            <div>
-              <div className="overflow-hidden">
-                <p className="about- bottom-anim text-sm">Contact</p>
-              </div>
-              <div className="flex gap-2 overflow-hidden">
-                <p className="bottom-anim font-medium uppercase">
-                  Envoyez un{" "}
-                  <a className="underline" href="mailto:augustin.briolon@gmail.com">
-                    mail
-                  </a>{" "}
-                  ou prenez{" "}
-                  <a
-                    className="underline"
-                    href="https://cal.com/augustin-briolon/30min?overlayCalendar=true"
-                    target="_blank"
-                  >
-                    rendez-vous
-                  </a>
-                </p>
-              </div>
+            <div className="overflow-hidden">
+              <p className="bottom-anim flex items-center gap-2 font-medium uppercase">
+                Disponible
+              </p>
+            </div>
+          </div>
+          <div>
+            <div className="overflow-hidden">
+              <p className="about- bottom-anim text-sm">Contact</p>
+            </div>
+            <div className="flex gap-2 overflow-hidden">
+              <p className="bottom-anim font-medium uppercase">
+                Envoyez un{" "}
+                <a className="underline" href="mailto:augustin.briolon@gmail.com">
+                  mail
+                </a>{" "}
+                ou prenez{" "}
+                <a
+                  className="underline"
+                  href="https://cal.com/augustin-briolon/30min?overlayCalendar=true"
+                  target="_blank"
+                >
+                  rendez-vous
+                </a>
+              </p>
             </div>
           </div>
         </div>
